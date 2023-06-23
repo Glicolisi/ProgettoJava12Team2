de47288f9cc470d91efecffda27dce4a009b22e2
Massimiliano Demma <demma.massimiliano@yahoo.it>
2023-06-23 17:02:56 +0200

    Progetto iniziato

---
+++ MechAppoint/src/main/java/com/Team2Java12/MechAppoint/MechAppointApplication.java
@@ -0,0 +1,13 @@
+package com.Team2Java12.MechAppoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechAppointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechAppointApplication.class, args);
+	}
+
+}
---
+++ MechAppoint/src/test/java/com/Team2Java12/MechAppoint/MechAppointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Team2Java12.MechAppoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechAppointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
