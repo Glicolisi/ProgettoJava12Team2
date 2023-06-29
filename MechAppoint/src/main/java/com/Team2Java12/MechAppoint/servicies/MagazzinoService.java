50cf8ba976a2d2c02252e7151da1d01e2d1d869a
Massimiliano Demma <demma.massimiliano@yahoo.it>
2023-06-29 17:42:22 +0200

    MagazzinoService

---
+++ MechAppoint/src/main/java/com/Team2Java12/MechAppoint/servicies/MagazzinoService.java
@@ -0,0 +1,30 @@
+package com.Team2Java12.MechAppoint.servicies;
+
+import com.Team2Java12.MechAppoint.entities.Magazzino;
+
+import com.Team2Java12.MechAppoint.repositories.MagazzinoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MagazzinoService{
+
+    @Autowired
+    private MagazzinoRepository magazzinoRepository;
+
+    public void createMagazzino (Magazzino magazzino) {
+        magazzinoRepository.save(magazzino);
+    }
+
+    public Magazzino getMagazzino (Integer magazzinoId) {
+        return magazzinoRepository.findById(magazzinoId).orElse(null);
+    }
+
+    public void updateMagazzino (Magazzino magazzino, Integer magazzinoId) {
+        magazzinoRepository.deleteById(magazzinoId);
+        magazzinoRepository.save(magazzino);
+    }
+
+    public void deleteMagazzino (Integer Id) {
+        magazzinoRepository.deleteById(Id);
+    }
+}
