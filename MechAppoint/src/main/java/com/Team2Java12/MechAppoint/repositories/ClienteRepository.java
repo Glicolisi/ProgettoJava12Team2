221e13ee126abb38dccfaeb934e8a70b198da44c
AndreaFusari <andrea.fusari@hotmail.com>
2023-07-04 14:48:44 +0200

    Corretto nome ClienteService

--- MechAppoint/src/main/java/com/Team2Java12/MechAppoint/repositories/ClienteRepository.java
+++ MechAppoint/src/main/java/com/Team2Java12/MechAppoint/repositories/ClienteRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClientiRepository extends JpaRepository<Cliente, Integer> {
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
 }
