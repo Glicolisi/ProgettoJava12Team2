1dc92da7ba8252e5025c0487640648f2290456b3
Massimiliano Demma <demma.massimiliano@yahoo.it>
2023-07-06 01:09:24 +0200

    Aggiornato pesantemente Service di Officina

--- MechAppoint/src/main/java/com/Team2Java12/MechAppoint/servicies/OfficinaService.java
+++ MechAppoint/src/main/java/com/Team2Java12/MechAppoint/servicies/OfficinaService.java
@@ -1,9 +1,16 @@
 package com.Team2Java12.MechAppoint.servicies;
 
+import com.Team2Java12.MechAppoint.Exception.ConflictException;
+import com.Team2Java12.MechAppoint.Exception.NotFoundException;
+import com.Team2Java12.MechAppoint.controllers.DTO.*;
 import com.Team2Java12.MechAppoint.entities.Officina;
+import com.Team2Java12.MechAppoint.entities.ValidationEnum;
 import com.Team2Java12.MechAppoint.repositories.OfficinaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 
@@ -11,30 +18,74 @@ public class OfficinaService {
 
     @Autowired
     private OfficinaRepository officinaRepository;
+    // TODO: Mettere a posto sta Propriety
+    //@Value("$app.feature.enablephysicaldeletion")
+    private boolean enablephysicaldeletion=false;
 
-    public void saveOfficina (Officina officina){
+    public CreateOfficinaResponseDto createOfficina (CreateOfficinaRequestDto request){
 
-        officinaRepository.save(officina);
-
-    }
-
-    public Officina getOfficina (Integer officinaid){
-
-        return officinaRepository.findById(officinaid).orElse(null);
-
-    }
-
-    public void updateOfficina (Officina officina, Integer officinaid){
-
-        officinaRepository.deleteById(officinaid);
-        officinaRepository.save(officina);
+       Optional<Officina> optionalOfficina = officinaRepository.findByNome(request.getNome());
+       optionalOfficina.orElseThrow(() -> new ConflictException());
+       Officina officina = new Officina(request.getNome(), request.getIndirizzo(), request.getEmail(), request.getValidation());
+       officina=officinaRepository.save(officina);
+       CreateOfficinaResponseDto createOfficinaResponseDto = new CreateOfficinaResponseDto();
+       createOfficinaResponseDto.setId(officina.getOfficinaid());
+       createOfficinaResponseDto.setStatus(ValidationEnum.OK);
+       return createOfficinaResponseDto;
 
     }
 
-    public void deleteOfficina (Integer officinaid){
 
-        officinaRepository.deleteById(officinaid);
+    public GetOfficinaResponseDto getOfficina(GetOfficinaRequestDto get) {
+        Optional<Officina> optionalOfficina;
+        if (get.getNome().isEmpty() && get.getId() == null) {
+            throw new NotFoundException("Campo di ricerca non inserito");
+        } else if (get.getNome().isEmpty()) {
+            optionalOfficina=officinaRepository.findById(get.getId());
+        } else if (get.getId() == null) {
+           optionalOfficina=officinaRepository.findByNome(get.getNome());
+        } else {
+            optionalOfficina=officinaRepository.findById(get.getId());
+        }
+
+        Officina officina= optionalOfficina.orElseThrow(()-> new NotFoundException("Parametri non trovati"));
+        return  new GetOfficinaResponseDto(officina.getOfficinaid(), officina.getNome(), officina.getIndirizzo(), officina.getEmail(), officina.getValidation());
 
     }
+
+    public BaseResponse updateOfficina(UpdateOfficinaRequestDto update){
+
+        Optional<Officina> optionalOfficina = officinaRepository.findByNome(update.getNome());
+        optionalOfficina.orElseThrow(() -> new ConflictException());
+        Officina officina = optionalOfficina.orElseThrow(()-> new NotFoundException("Parametri non trovati"));
+        officina.setNome(update.getNome());
+        officina.setIndirizzo(update.getIndirizzo());
+        officina.setEmail(update.getEmail());
+
+        return  new BaseResponse();
+    }
+
+
+    public BaseResponse deleteOfficina (DeleteOfficinaRequestDto delete){
+
+    Optional<Officina> optionalOfficina = officinaRepository.findById(delete.getId());
+    if(optionalOfficina.isEmpty()){
+        throw new RuntimeException("L'oggetto non esiste");
+    }
+    Officina officina = optionalOfficina.get();
+
+        if(enablephysicaldeletion){
+            officinaRepository.delete(officina);
+        } else{
+            officina.setValidation(ValidationEnum.DELETED);
+            officinaRepository.save(officina);
+        }
+    BaseResponse baseResponse = new BaseResponse();
+    baseResponse.setStatus(ValidationEnum.OK);
+    return baseResponse;
+
+    }
+
+
 
 }
