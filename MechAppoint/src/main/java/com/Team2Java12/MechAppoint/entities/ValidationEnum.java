942a221f78a76bf804fee4ab2d90e58db3819056
Massimiliano Demma <demma.massimiliano@yahoo.it>
2023-07-06 17:12:22 +0200

    Formattato Enum

--- MechAppoint/src/main/java/com/Team2Java12/MechAppoint/entities/ValidationEnum.java
+++ MechAppoint/src/main/java/com/Team2Java12/MechAppoint/entities/ValidationEnum.java
@@ -2,18 +2,18 @@ package com.Team2Java12.MechAppoint.entities;
 
 public enum ValidationEnum {
 
-    ACTIVE ("Active"),
-    DELETED ("Deleted"),
-    OK ("Ok"),
-    KO ("Ko");
+    ACTIVE("Active"),
+    DELETED("Deleted"),
+    OK("Ok"),
+    KO("Ko");
 
     private String status;
 
-    ValidationEnum (String status){
-        this.status=status;
+    ValidationEnum(String status) {
+        this.status = status;
     }
 
-    public String getStatus(){
+    public String getStatus() {
         return status;
     }
 
