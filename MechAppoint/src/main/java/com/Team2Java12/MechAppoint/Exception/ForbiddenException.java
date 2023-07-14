7c247174a629e339cf8dd6a9fdd871971d811897
Massimiliano Demma <demma.massimiliano@yahoo.it>
2023-07-14 12:37:41 +0200

    Aggiunte ulteriori Eccezioni

---
+++ MechAppoint/src/main/java/com/Team2Java12/MechAppoint/Exception/ForbiddenException.java
@@ -0,0 +1,11 @@
+package com.Team2Java12.MechAppoint.Exception;
+
+public class ForbiddenException extends RuntimeException {
+
+    private static final long serialVersionUID = -3L;
+    public ForbiddenException (String message) {
+        super(message);
+    }
+
+
+}
--- MechAppoint/src/main/java/com/Team2Java12/MechAppoint/advice/ControllerAdvice.java
+++ MechAppoint/src/main/java/com/Team2Java12/MechAppoint/advice/ControllerAdvice.java
@@ -1,5 +1,8 @@
 package com.Team2Java12.MechAppoint.advice;
 
+import com.Team2Java12.MechAppoint.Exception.ConflictException;
+import com.Team2Java12.MechAppoint.Exception.ForbiddenException;
+import com.Team2Java12.MechAppoint.Exception.NotExistsException;
 import com.Team2Java12.MechAppoint.Exception.NotFoundException;
 import com.Team2Java12.MechAppoint.controllers.DTO.BaseResponse;
 import com.Team2Java12.MechAppoint.dataStatus.ValidationEnum;
@@ -10,7 +13,34 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerAdvice {
-    //TODO: Gestire anche altre eccezione
+
+    @ExceptionHandler(value = {ConflictException.class})
+    @ResponseStatus(value = HttpStatus.CONFLICT)
+    public BaseResponse hanglingAlredyExist(ConflictException e) {
+        BaseResponse br = new BaseResponse();
+        br.setStatus(ValidationEnum.KO);
+        br.setErrorMessage("RESOURCE_ALREADY_EXIST");
+        return  br;
+    }
+
+    @ExceptionHandler(value = {ForbiddenException.class})
+    @ResponseStatus(value = HttpStatus.FORBIDDEN)
+    public BaseResponse hanglingForbidden(ConflictException e) {
+        BaseResponse br = new BaseResponse();
+        br.setStatus(ValidationEnum.KO);
+        br.setErrorMessage("RESOURCE_FORBIDDEN");
+        return  br;
+    }
+
+    @ExceptionHandler(value = {NotExistsException.class})
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public BaseResponse hanglingNotExists(ConflictException e) {
+        BaseResponse br = new BaseResponse();
+        br.setStatus(ValidationEnum.KO);
+        br.setErrorMessage("RESOURCE_NOT_EXISTS");
+        return  br;
+    }
+
     @ExceptionHandler(value = {NotFoundException.class})
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public BaseResponse hanglingNotFound(NotFoundException e) {
